6fcfaa3516f818339b1088355c01fe0c4eaf59a6
Vansh <vansh200622@gmail.com>
2026-05-25 16:16:22 +0530

    Add

--- team-task-manager/frontend/src/services/api.js
+++ team-task-manager/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "https://team-task-manager-project-8c2d.onrender.com",
   withCredentials: true,
 });
 
