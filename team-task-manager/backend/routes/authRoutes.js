05c01ed6d08045849c8cc6fad097729f0bf0dae6
Vansh <vansh200622@gmail.com>
2026-05-25 16:07:09 +0530

    add

--- team-task-manager/backend/routes/authRoutes.js
+++ team-task-manager/backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 
